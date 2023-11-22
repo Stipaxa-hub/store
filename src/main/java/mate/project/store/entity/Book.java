112cd4049b96d697af55687874efcf1e22e03d17
Stipaxa <petrivs572@gmail.com>
2023-11-22 17:07:02 +0200

    Created Book entity

--- src/main/java/mate/project/store/entity/Book.java
+++ src/main/java/mate/project/store/entity/Book.java
@@ -5,25 +5,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@Table(name = "books")
+@SQLDelete(sql = "UPDATE books SET is_deleted = TRUE WHERE id = ?")
+@Where(clause = "is_deleted = FALSE")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
+    @Column(nullable = false)
     private String title;
     @NotNull
+    @Column(nullable = false)
     private String author;
     @NotNull
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String isbn;
     @NotNull
+    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
