7c87aab9f5ddc250eed7e97be797e64db9e5310e
Stipaxa <petrivs572@gmail.com>
2023-11-22 17:50:46 +0200

    Fixed book entity and created CommandLineRunner bean

--- src/main/java/mate/project/store/entity/Book.java
+++ src/main/java/mate/project/store/entity/Book.java
@@ -1,47 +1,27 @@
 package mate.project.store.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.UniqueElements;
 
 @Entity
-@Table(name = "books")
-@SQLDelete(sql = "UPDATE books SET is_deleted = TRUE WHERE id = ?")
-@Where(clause = "is_deleted = FALSE")
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    @Column(nullable = false)
-    private String title;
-    @NotNull
-    @Column(nullable = false)
     private String author;
     @NotNull
-    @Column(nullable = false, unique = true)
+    @UniqueElements
     private String isbn;
     @NotNull
-    @Column(nullable = false)
     private BigDecimal price;
     private String description;
     private String coverImage;
-
-    @Column(name = "is_deleted", nullable = false)
-    private boolean isDeleted = false;
 }
