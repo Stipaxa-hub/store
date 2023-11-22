0456af978b9085647bf368cfa7839e7f499c3bd0
Stipaxa <petrivs572@gmail.com>
2023-11-22 13:44:04 +0200

    Renamed structure package

--- src/main/java/mate/project/store/StoreApplication.java
+++ src/main/java/mate/project/store/StoreApplication.java
@@ -1,4 +1,4 @@
-package mate.store.store;
+package mate.project.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/mate/project/store/StoreApplicationTests.java
+++ src/test/java/mate/project/store/StoreApplicationTests.java
@@ -1,4 +1,4 @@
-package mate.store.store;
+package mate.project.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
