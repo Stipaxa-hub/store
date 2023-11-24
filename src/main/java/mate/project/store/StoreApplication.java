ddd216288842d58a833e39df06af6e10e44eaad7
Stipaxa <petrivs572@gmail.com>
2023-11-24 15:26:15 +0200

    Fixed

--- src/main/java/mate/project/store/StoreApplication.java
+++ src/main/java/mate/project/store/StoreApplication.java
@@ -1,13 +1,7 @@
 package mate.project.store;
 
-import java.math.BigDecimal;
-import mate.project.store.entity.Book;
-import mate.project.store.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreApplication {
