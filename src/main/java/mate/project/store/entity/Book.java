8d08cfad0773a19c26954be258a791349c312f9b
Stipaxa <petrivs572@gmail.com>
2023-12-02 12:00:56 +0200

    Fixed bugs

--- src/main/java/mate/project/store/entity/Book.java
+++ src/main/java/mate/project/store/entity/Book.java
@@ -12,9 +12,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "books")
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false")
 @Data
 @Builder
 @NoArgsConstructor
@@ -37,4 +41,7 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
