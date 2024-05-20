6a9e27b58a84f2aa7bd4b129834362a675ba0e3c
Stepan <petrivs572@gmail.com>
2024-05-20 23:32:33 +0300

    Added BookControllerTest

--- src/test/java/mate/project/store/controller/BookControllerTest.java
+++ src/test/java/mate/project/store/controller/BookControllerTest.java
@@ -1,8 +1,10 @@
 package mate.project.store.controller;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import mate.project.store.dto.book.BookDto;
+import mate.project.store.dto.book.BookSearchParametersDto;
 import mate.project.store.dto.book.CreateBookRequestDto;
 import mate.project.store.entity.Category;
 import mate.project.store.repository.category.CategoryRepository;
@@ -135,6 +138,174 @@ class BookControllerTest {
                 .getContentAsByteArray(), BookDto[].class);
         Assertions.assertEquals(5, actual.length);
         Assertions.assertEquals(expected, Arrays.stream(actual).toList());
+    }
+
+    @WithMockUser(username = "user")
+    @Test
+    @Sql(scripts = {
+            "classpath:database/books/add-mock-books-to-books-table.sql",
+            "classpath:database/categories/add-mock-categories-to-categories-table.sql",
+            "classpath:database/books_categories/"
+                    + "add-mock-book-category-to-books_categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            "classpath:database/books_categories/"
+                    + "remove-mock-book_categories-from-book_categories-table.sql",
+            "classpath:database/books/remove-mock-books-from-books-table.sql",
+            "classpath:database/categories/remove-mock-categories-from-categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Get book by id")
+    void getBookById_ValidId_ShouldReturnValidBookDto() throws Exception {
+        // Given
+        Long id = 1L;
+        BookDto expectedBookDto = new BookDto(
+                id, "Book 1", "Author 1", "34-5346-4h-4561",
+                BigDecimal.valueOf(1), "Description 1", "image1", Set.of(1L));
+        // When
+        MvcResult result = mockMvc.perform(
+                get("/books/{id}", id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        // Then
+        BookDto actulBookDto = objectMapper.readValue(
+                result.getResponse().getContentAsString(), BookDto.class
+        );
+        Assertions.assertNotNull(actulBookDto);
+        EqualsBuilder.reflectionEquals(expectedBookDto, actulBookDto, "id");
+        Assertions.assertEquals(expectedBookDto, actulBookDto);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @Sql(scripts = {
+            "classpath:database/books/add-mock-books-to-books-table.sql",
+            "classpath:database/categories/add-mock-categories-to-categories-table.sql",
+            "classpath:database/books_categories/"
+                    + "add-mock-book-category-to-books_categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            "classpath:database/books_categories/"
+                    + "remove-mock-book_categories-from-book_categories-table.sql",
+            "classpath:database/books/remove-mock-books-from-books-table.sql",
+            "classpath:database/categories/remove-mock-categories-from-categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Update book by id")
+    void update_ValidRequest_ShouldReturnUpdatedBookDto() throws Exception {
+        // Given
+        Long id = 2L;
+        CreateBookRequestDto updateBookRequestDto = CreateBookRequestDto.builder()
+                .title("Updated Title")
+                .author("Updated Author")
+                .isbn("978-161-729-045-9")
+                .price(BigDecimal.valueOf(100))
+                .description("Updated Description")
+                .coverImage("Updated CoverImage")
+                .categoriesIds(Set.of(2L))
+                .build();
+
+        BookDto expectedBookDto = BookDto.builder()
+                .id(id)
+                .title(updateBookRequestDto.getTitle())
+                .author(updateBookRequestDto.getAuthor())
+                .isbn(updateBookRequestDto.getIsbn())
+                .price(updateBookRequestDto.getPrice())
+                .description(updateBookRequestDto.getDescription())
+                .coverImage(updateBookRequestDto.getCoverImage())
+                .categoriesIds(updateBookRequestDto.getCategoriesIds())
+                .build();
+
+        String jsonRequest = objectMapper.writeValueAsString(updateBookRequestDto);
+
+        // When
+        MvcResult result = mockMvc.perform(
+                put("/books/{id}", id)
+                        .content(jsonRequest)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        // Then
+        BookDto actualBookDto = objectMapper.readValue(
+                result.getResponse().getContentAsString(), BookDto.class
+        );
+        Assertions.assertNotNull(actualBookDto);
+        EqualsBuilder.reflectionEquals(expectedBookDto, actualBookDto, "id");
+        Assertions.assertEquals(expectedBookDto, actualBookDto);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    @Sql(scripts = {
+            "classpath:database/books/add-mock-books-to-books-table.sql",
+            "classpath:database/categories/add-mock-categories-to-categories-table.sql",
+            "classpath:database/books_categories/"
+                    + "add-mock-book-category-to-books_categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            "classpath:database/books_categories/"
+                    + "remove-mock-book_categories-from-book_categories-table.sql",
+            "classpath:database/books/remove-mock-books-from-books-table.sql",
+            "classpath:database/categories/remove-mock-categories-from-categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Delete book by id")
+    void delete_ValidId_ShouldDeleteCorrectBook() throws Exception {
+        // Given
+        Long id = 3L;
+
+        // When
+        mockMvc.perform(delete("/books/{id}", id))
+                .andExpect(status().isNoContent())
+                .andReturn();
+    }
+
+    @Test
+    @WithMockUser(username = "user")
+    @Sql(scripts = {
+            "classpath:database/books/add-mock-books-to-books-table.sql",
+            "classpath:database/categories/add-mock-categories-to-categories-table.sql",
+            "classpath:database/books_categories/"
+                    + "add-mock-book-category-to-books_categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = {
+            "classpath:database/books_categories/"
+                    + "remove-mock-book_categories-from-book_categories-table.sql",
+            "classpath:database/books/remove-mock-books-from-books-table.sql",
+            "classpath:database/categories/remove-mock-categories-from-categories-table.sql"
+    }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("Search books by parameters")
+    void searchBooks_ValidBookSearchParametersDto_ShouldReturnValidBookDto() throws Exception {
+        // Given
+        BookDto expectedBookDto = BookDto.builder()
+                .id(1L)
+                .title("Book 1")
+                .author("Author 1")
+                .isbn("34-5346-4h-4561")
+                .price(BigDecimal.valueOf(1))
+                .description("Description 1")
+                .coverImage("image1")
+                .categoriesIds(Set.of(1L))
+                .build();
+
+        BookSearchParametersDto bookSearchParametersDto = new BookSearchParametersDto(
+                new String[]{"Author 1"},
+                new String[]{""}
+        );
+        String jsonRequest = objectMapper.writeValueAsString(bookSearchParametersDto);
+        // When
+        MvcResult result = mockMvc.perform(get("/books/search")
+                .content(jsonRequest)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        // Then
+        List<BookDto> actualBookDtoList = Arrays.asList(
+                objectMapper.readValue(result.getResponse().getContentAsString(), BookDto[].class)
+        );
+        BookDto actualBookDto = actualBookDtoList.get(0);
+        Assertions.assertNotNull(actualBookDto);
+        EqualsBuilder.reflectionEquals(expectedBookDto, actualBookDto, "id");
+        Assertions.assertEquals(expectedBookDto, actualBookDto);
     }
 
     private List<BookDto> getBookDtoListMock() {
